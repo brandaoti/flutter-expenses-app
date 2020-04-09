536389d0bf96c2d206f42943eca7d1d1a0d32317
brandaoti <dannnbrandao@gmail.com>
2020-04-09 15:12:50 -0300

    add expenses_app, atualizado main e material

---
+++ lib/components/expenses_app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Implements Scaffold
+    return Scaffold();
+  }
+}
--- lib/material.dart
+++ lib/material.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import './components/expenses_app.dart';
 
 class TransactionMaterial extends StatelessWidget {
-  
+  //
   @override
   Widget build(BuildContext context) {
-    // Build Material 
-    return null;
+    // Build Material
+    return MaterialApp(
+      home: ExpensesApp(),
+    );
   }
 }
