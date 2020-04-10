c3b2ee175b9c228c10b0509fc0abd441aab29963
brandaoti <dannnbrandao@gmail.com>
2020-04-09 22:39:32 -0300

    Add pacote intl no pub

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -14,16 +14,21 @@ class TransactionList extends StatelessWidget {
         return Card(
           elevation: 5,
           child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Container(
-                height: 300,
                 color: Colors.purple[200],
-                child: Text('R\$ ${tr.value.toStringAsFixed(2)}'),
+                child: Text(
+                  'R\$ ${tr.value.toStringAsFixed(2)}',
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
+              // Implementação titulo e data
               Column(
                 children: <Widget>[
-                  Text('${tr.title}'),
-                  Text('${tr.date}'),
+                  Text(tr.title),
+                  //
+                  Text(''),
                 ],
               ),
             ],
