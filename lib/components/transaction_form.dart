2b9758d3cf2cd72f8a046b742c2d9a4330ec9d32
brandaoti <dannnbrandao@gmail.com>
2020-04-13 23:01:21 -0300

    Corrigindo formulario que apagava os valores

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
+class TransactionForm extends StatefulWidget {
   // Controladores
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
-  //
   final void Function(String, double) onSubmit;
 
   TransactionForm(this.onSubmit);
+
+  @override
+  _TransactionFormState createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+
+  final valueController = TextEditingController();
 
   _submitForm() {
     final title = titleController.text;
     final value = double.tryParse(valueController.text) ?? 0.0;
 
     if (title.isEmpty || value <= 0) {
-      return AlertDialog(
-        title: Text('Campo vazio'),
-      );
+      return;
     }
 
-    onSubmit(title, value);
+    widget.onSubmit(title, value);
   }
 
   @override
